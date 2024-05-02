04263e823b8d7ea08783f15c7e34471bedaf70b4
Денис Левченко <denislevcenko@MacBook-Air-Denis.local>
2024-05-02 16:29:12 +0300

    Added Spring configuration for debugging in IDE

---
+++ config/spring.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+Spring.after_fork do
+  ENV['DEBUGGER_STORED_RUBYLIB']&.split(File::PATH_SEPARATOR)&.each do |path|
+    next unless path =~ /ruby-debug-ide/
+
+    load "#{path}/ruby-debug-ide/multiprocess/starter.rb"
+  end
+end
